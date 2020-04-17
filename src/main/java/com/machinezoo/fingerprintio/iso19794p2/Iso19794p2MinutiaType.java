e22a48a6fb2d3c6a09c9e7f3576ef1277637133d
Robert Vazan <robvcode@gmail.com>
2020-04-17 06:45:43 +0000

    Minimal decoder for ISO 19794-2

---
+++ src/main/java/com/machinezoo/fingerprintio/iso19794p2/Iso19794p2MinutiaType.java
@@ -0,0 +1,8 @@
+// Part of FingerprintIO: https://fingerprintio.machinezoo.com
+package com.machinezoo.fingerprintio.iso19794p2;
+
+public enum Iso19794p2MinutiaType {
+	OTHER,
+	ENDING,
+	BIFURCATION;
+}
---
+++ src/main/java/com/machinezoo/fingerprintio/iso19794p2/Iso19794p2Position.java
@@ -0,0 +1,16 @@
+// Part of FingerprintIO: https://fingerprintio.machinezoo.com
+package com.machinezoo.fingerprintio.iso19794p2;
+
+public enum Iso19794p2Position {
+	UNKNOWN,
+	RIGHT_THUMB,
+	RIGHT_INDEX,
+	RIGHT_MIDDLE,
+	RIGHT_RING,
+	RIGHT_LITTLE,
+	LEFT_THUMB,
+	LEFT_INDEX,
+	LEFT_MIDDLE,
+	LEFT_RING,
+	LEFT_LITTLE;
+}
